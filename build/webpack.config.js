82cd2ed75f2612c9e32cb266d69ec26ff88e7a31
Naldo Wang <naldo_wang9641@qq.com>
2020-07-22 18:02:54 +0800

    初始提交

---
+++ build/webpack.config.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, '../dist')
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.resolve(__dirname, '../public'),
+        port: 9000,
+        hot: true,
+        open: false
+    },
+    plugins: [
+        new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+        new HtmlWebpackPlugin({
+            template: 'public/index.html'
+        })
+    ]
+}
